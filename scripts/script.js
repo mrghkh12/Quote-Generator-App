8c988bed110da841f5fbc262033e308d0c3c95f9
mohammad reza <mrghkh12@gmail.com>
2024-07-15 14:41:34 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
-const $ = document
+const $ = document;
 
-const quoteTextElem = $.querySelector('.quote-text')
-const quoteAuthorElem = $.querySelector('.quote-author')
-const newQuoteBtn = $.querySelector('.newQuoteBtn')
-const postQuoteBtn = $.querySelector('.postQuoteBtn')
+const quoteTextElem = $.querySelector(".quote-text");
+const quoteAuthorElem = $.querySelector(".quote-author");
+const newQuoteBtn = $.querySelector(".newQuoteBtn");
+const postQuoteBtn = $.querySelector(".postQuoteBtn");
 
-async function getQuote(){
-    let apiUrl = 'https://api.quotable.io/random'
+async function getQuote() {
+  let apiUrl = "https://api.quotable.io/random";
 
-    let response = await fetch(apiUrl)
-    let data = await response.json()
+  let response = await fetch(apiUrl);
+  let data = await response.json();
 
-    setQuoteDate(data)
+  setQuoteDate(data);
 }
 
-function setQuoteDate(quoteData){
-    quoteTextElem.innerHTML = quoteData.content
-    quoteAuthorElem.innerHTML = quoteData.author
+function setQuoteDate(quoteData) {
+  quoteTextElem.innerHTML = quoteData.content;
+  quoteAuthorElem.innerHTML = quoteData.author;
 }
 
-function postQuote(){
-    window.open(`https://x.com/intent/post?text=${quoteTextElem.innerHTML} ---- by ${quoteAuthorElem.innerHTML}`,'X Window', 'width=600, height=300')
+function postQuote() {
+  window.open(
+    `https://x.com/intent/post?text=${quoteTextElem.innerHTML} ---- by ${quoteAuthorElem.innerHTML}`,
+    "X Window",
+    "width=600, height=300"
+  );
 }
 
-newQuoteBtn.addEventListener('click' , getQuote)
-postQuoteBtn.addEventListener('click' , postQuote)
-window.addEventListener('load' , getQuote)
+newQuoteBtn.addEventListener("click", getQuote);
+postQuoteBtn.addEventListener("click", postQuote);
+window.addEventListener("load", getQuote);
