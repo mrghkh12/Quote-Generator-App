8d14c470ed8c938b8b2fb5a7a5f471194184d4f6
mohammad reza <mrghkh12@gmail.com>
2024-07-15 13:51:56 +0330

    feat(script.js): select elements

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const $ = document
+
+const quoteTextElem = $.querySelector('.quote-text')
+const quoteAuthorElem = $.querySelector('.quote-author')
+const newQuoteBtn = $.querySelector('.newQuoteBtn')
+const postQuoteBtn = $.querySelector('.postQuoteBtn')
