ac9d618e5b6d186b7adeeae2710ece53e9a7f357
mohammad reza <mrghkh12@gmail.com>
2024-07-15 13:54:25 +0330

    feat(script.js): requst for quote api

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,11 @@ const quoteTextElem = $.querySelector('.quote-text')
 const quoteAuthorElem = $.querySelector('.quote-author')
 const newQuoteBtn = $.querySelector('.newQuoteBtn')
 const postQuoteBtn = $.querySelector('.postQuoteBtn')
+
+async function getQuote(){
+    let apiUrl = 'https://api.quotable.io/random'
+
+    let response = await fetch(apiUrl)
+    let data = await response.json()
+
+}
